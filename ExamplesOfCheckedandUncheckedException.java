c4d854178a5129d02cf772463b942003af76b256
pankajy242 <58069813+pankajy242@users.noreply.github.com>
2021-09-21 17:10:04 +0530

    Add files via upload

---
+++ ExamplesOfCheckedandUncheckedException.java
@@ -0,0 +1,35 @@
+Example of Checked Exception
+
+
+--- IOException
+--- FileNotFoundException
+--- InterruptedException
+--- RunnableException
+--- InvalidClassException 
+--- InvalidObjectException
+--- NotSerializableException
+--- OptionalDataException
+--- UncheckedIOException
+
+
+
+
+
+
+
+
+Example of Unchecked Exception
+
+
+--- ArrayIndexOutOfBoundException
+--- ClassCastException
+--- NullPointerException
+--- StringIndexOutOfBoundException
+--- IllegalArgumentException
+--- NumberFormateException
+--- StackOverFlow
+--- OutOfMemory
+--- NoClassDefFoundException
+--- ExceptionInInitialzer
+--- AssertionError
+
